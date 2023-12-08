73f8586a6807642638abc5e2417e962cc6b5a133
vandriichenko <victoria.skrypkina@gmail.com>
2023-12-08 12:37:19 +0000

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 50f;
+    public float jumpForce = 100f;
+    public float fallMultiplier = 10f;  // Додатковий множник гравітації під час падіння
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
+
+        // Якщо гравець падає, застосуйте додаткову гравітацію
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveX, moveY) * speed * Time.deltaTime;
+        rb.MovePosition(rb.position + movement);
+    }
+
+    void Jump()
+    {
+        Debug.Log("Jumping");
+        rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+{
+    if (collision.gameObject.tag == "Ground")
+    {
+        isGrounded = false;
+    }
+}
+
+}
